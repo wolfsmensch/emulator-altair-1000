a9b9c9a9e68e31e78c8cb10e8e96cdf4c95b9973
Евгений Троценко <evenguy@yandex.ru>
2020-04-24 19:21:55 +0600

    Создано тело класса CPU

---
+++ Altair-1000/devices/CPU/CPU.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altair_1000.devices.CPU
+{
+    /// <summary>
+    /// Центральный процессор
+    /// </summary>
+    public class CPU
+    {
+        // Список команд
+        // Регистры
+
+        /// <summary>
+        /// Указатель на адрес текущей инструкции
+        /// </summary>
+        Byte AddrPointer;
+
+        /// <summary>
+        /// Кол-во выполненных тактов
+        /// </summary>
+        UInt32 CyclesCnt;
+        
+        public RAM RAM = null;
+
+        public CPU(RAM RAM)
+        {
+            this.RAM = RAM;
+            if ((RAM == null) || (RAM.Size <= 0))
+                throw new Exception("Не удалось подключиться к устройству оперативной памяти");
+            
+            AddrPointer = 0;
+            CyclesCnt = 0;
+        }
+
+        /// <summary>
+        /// Выполнение такта (цикла) процессора
+        /// </summary>
+        public void Clock()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
