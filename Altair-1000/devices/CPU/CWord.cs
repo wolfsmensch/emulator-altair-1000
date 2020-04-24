0690302c6841850307cf962cd18d25f3fa0522b9
Евгений Троценко <evenguy@yandex.ru>
2020-04-24 17:11:09 +0600

    Описан класс машинного слова

---
+++ Altair-1000/devices/CPU/CWord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altair_1000.devices.CPU
+{
+    /// <summary>
+    /// Машинное слово (8 бит)
+    /// </summary>
+    public class CWord
+    {
+        public static readonly int Capacity = 8;
+        public List<Boolean> Data = new List<bool>(Capacity);
+
+        public CWord()
+        {
+            for (int i = 0; i < Capacity; i++)
+                Data[i] = false;
+        }
+    }
+}
---
+++ Altair-1000/devices/RAM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altair_1000.devices
+{
+    class RAM
+    {
+
+    }
+}
